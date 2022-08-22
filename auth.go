13a5c594d4e6ffccc99f493ca11148f969ffda08
chris <fellowship>
2022-08-22 01:51:20 +0000

    Add token generation

--- auth.go
+++ auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
@@ -20,28 +21,42 @@ func erebor(s string) string {
 	return status
 }
 
+func token() string {
+	b := make([]byte, 10)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
 func adduser(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	pw := r.URL.Query().Get("pw")
-	fmt.Fprintln(w, "id =>", id)
-	fmt.Fprintln(w, "pw =>", pw)
 	h := sha256.Sum256([]byte(pw))
 	s := fmt.Sprintf("set user %s %x", id, h)
-	fmt.Fprint(w, erebor(s))
+	e := strings.TrimRight(erebor(s), "\n")
+	if e == "OK" {
+		fmt.Fprintf(w, "{\"user\":\"%s\"}\n", id)
+		fmt.Println("adduser", id, "=> success")
+	} else {
+		fmt.Fprintf(w, "{\"error\":\"Failed to add user\"}\n")
+		fmt.Println("adduser", id, "=> failed")
+	}
 }
 
 func auth(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	pw := r.URL.Query().Get("pw")
-	fmt.Fprintln(w, "id =>", id)
-	fmt.Fprintln(w, "pw =>", pw)
 	h := sha256.Sum256([]byte(pw))
 	s := fmt.Sprintf("get user %s", id)
 	auth := erebor(s)
 	if strings.TrimRight(auth, "\n") == hex.EncodeToString(h[:]) {
-		fmt.Fprintln(w, "success")
+		t := token()
+		s := fmt.Sprintf("set token %s %s", id, t)
+		erebor(s)
+		fmt.Fprintf(w, "{\"token\":\"%s\"}\n", t)
+		fmt.Println("auth", id, "=> success")
 	} else {
-		fmt.Fprintln(w, "authentication failed")
+		fmt.Fprintln(w, "{\"error\":\"Authentication failed\"}")
+		fmt.Println("auth", id, "=> failed")
 	}
 }
 
