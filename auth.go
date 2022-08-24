43a88b9133a4dcf212ef733731aad68eb236f1e6
chris <fellowship>
2022-08-24 20:53:51 +0000

    Add deluser and close erebor connections

--- auth.go
+++ auth.go
@@ -17,6 +17,7 @@ func erebor(s string) string {
 	if err != nil {
 		return "(error): could not connect to erebor\n"
 	}
+	defer conn.Close()
 	fmt.Fprintf(conn, s)
 	status, err := bufio.NewReader(conn).ReadString('\n')
 	return strings.TrimRight(status, "\n")
@@ -31,6 +32,11 @@ func token() string {
 func adduser(w http.ResponseWriter, r *http.Request) {
 	id := r.URL.Query().Get("id")
 	pw := r.URL.Query().Get("pw")
+	if id == "" || pw == "" {
+		fmt.Fprintf(w, "{\"error\":\"Missing username or password\"}\n")
+		log.Println("adduser", id, "=> failed")
+		return
+	}
 	h := sha256.Sum256([]byte(pw))
 	s := fmt.Sprintf("set user %s %x", id, h)
 	e := erebor(s)
@@ -42,6 +48,25 @@ func adduser(w http.ResponseWriter, r *http.Request) {
 	} else {
 		fmt.Fprintf(w, "{\"error\":\"Failed to add user\"}\n")
 		log.Println("adduser", id, "=> failed")
+	}
+}
+
+func deluser(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	tk := r.URL.Query().Get("tk")
+	h := sha256.Sum256([]byte(tk))
+	s := fmt.Sprintf("get token %s", id)
+	auth := erebor(s)
+	if auth == hex.EncodeToString(h[:]) {
+		s := fmt.Sprintf("del user %s", id)
+		erebor(s)
+		s = fmt.Sprintf("del token %s", id)
+		erebor(s)
+		fmt.Fprintf(w, "{\"user\":\"%s\"}\n", id)
+		log.Println("deluser", id, "=> success")
+	} else {
+		fmt.Fprintln(w, "{\"error\":\"Invalid username or token\"}")
+		log.Println("deluser", id, "=> failed")
 	}
 }
 
@@ -65,6 +90,7 @@ func auth(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/api/adduser", adduser)
+	http.HandleFunc("/api/deluser", deluser)
 	http.HandleFunc("/api/auth", auth)
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
